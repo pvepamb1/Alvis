fc3c7363350d19ca58e82840e73f7e7f1b9c4eda
Prasenna Venkatesh Vepambedu <prasennavenkatesh@gmail.com>
2019-01-30 00:33:44 +0530

    Another behemoth commit
     - Add debian template files necessary to build deb package
     - Add gradle tasks to build debian source package
     - Add shell scripts to decrypt keys, sign the package and upload to
    launchpad
     - Add encrypted keys [both gpg and ssh]
     - Move travis properties from application properties to separate file
     - Modify travis file to run scripts

---
+++ gradle/debian/menu.ex
@@ -0,0 +1,2 @@
+?package(homeauto):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="homeauto" command="/usr/bin/homeauto"
--- src/test/java/com/automation/HomeAutomationJavaApplicationTests.java
+++ src/test/java/com/automation/HomeAutomationJavaApplicationTests.java
@@ -3,10 +3,14 @@ package com.automation;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@TestPropertySource(locations="classpath:travis.properties")
+@ContextConfiguration
 public class HomeAutomationJavaApplicationTests {
 
 	@Test
