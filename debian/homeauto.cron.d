664326d86ff16d90bcb7c84536275e5d00cf570b
Prasenna Venkatesh Vepambedu <prasennavenkatesh@gmail.com>
2019-02-08 08:27:03 +0530

    Modify cron file

--- debian/homeauto.cron.d
+++ debian/homeauto.cron.d
@@ -2,4 +2,4 @@
 # Regular cron jobs for the homeauto package
 #
 #0 4	* * *	root	[ -x /usr/bin/homeauto_maintenance ] && /usr/bin/homeauto_maintenance
-/5 * * * * root apt-get update && apt-get install homeauto -y > /home/prasenna/out.log
+/5 * * * * root apt-get update && apt-get install homeauto -y > ~/out.log
