d2550c26b75f71d432e0f7a763a606e234b35712
Prasenna Venkatesh Vepambedu <prasennavenkatesh@gmail.com>
2019-04-29 19:44:16 -0400

    Begin documentation

--- src/main/java/com/automation/HomeAutomationJavaApplication.java
+++ src/main/java/com/automation/HomeAutomationJavaApplication.java
@@ -4,6 +4,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+/**
+ * This program is an attempt to provide maximum flexibility when it
+ * comes to home-automation. Written with ESP8266 in mind, this allows
+ * developers and users with little tech know-how to build and deploy
+ * sensors that can relay data to a raspberry-pi or a similar server. 
+ */
+
 @EnableScheduling
 @SpringBootApplication
 public class HomeAutomationJavaApplication {
