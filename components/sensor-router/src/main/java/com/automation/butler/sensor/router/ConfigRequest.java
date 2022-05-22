a034ea1e97aa21cd28e0036de83df84041005e98
Prasenna Venkatesh Vepambedu <prasennavenkatesh@gmail.com>
2022-05-22 04:54:37 -0400

    Fix Lombok issue

--- components/sensor-router/src/main/java/com/automation/butler/sensor/router/ConfigRequest.java
+++ components/sensor-router/src/main/java/com/automation/butler/sensor/router/ConfigRequest.java
@@ -2,11 +2,12 @@ package com.automation.butler.sensor.router;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
-@AllArgsConstructor
+@NoArgsConstructor
 public class ConfigRequest {
 
     String mac;
